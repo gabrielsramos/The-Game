e34cca58321b2946d42dc19a5a23f86c8b4a0083
Gabriel Ramos <gahbrielramos@gmail.com>
2023-02-11 15:51:14 -0300

    Add camera behaviour, rotation and zoom.

---
+++ Assets/Scripts/CameraBehaviour/CameraRotation.cs
@@ -0,0 +1,53 @@
+using Cinemachine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraRotation : MonoBehaviour
+{
+    [SerializeField] private InputAction mouseInputAction;
+    [SerializeField] private float rotationVelocity = 200;
+
+    private CinemachineFreeLook freeLookCamera;
+
+    private void Start()
+    {
+        freeLookCamera = GetComponent<CinemachineFreeLook>();
+    }
+
+    private void OnEnable()
+    {
+        mouseInputAction.Enable();
+        mouseInputAction.started += StartRotation;
+        mouseInputAction.canceled += FinishRotation;
+    }
+
+    private void OnDisable()
+    {
+        mouseInputAction.started -= StartRotation;
+        mouseInputAction.canceled -= FinishRotation;
+        mouseInputAction.Disable();
+    }
+
+    private void Update()
+    {
+        if (Input.mouseScrollDelta.y != 0)
+        {
+            freeLookCamera.m_YAxis.m_MaxSpeed = 10;
+        }
+    }
+
+    private void StartRotation(InputAction.CallbackContext context)
+    {
+        Debug.Log("[Camera] Started rotation");
+        freeLookCamera.m_XAxis.m_MaxSpeed = rotationVelocity;
+    }
+
+    private void FinishRotation(InputAction.CallbackContext context)
+    {
+        Debug.Log("[Camera] Finished rotation");
+        freeLookCamera.m_XAxis.m_MaxSpeed = 0;
+    }
+}
